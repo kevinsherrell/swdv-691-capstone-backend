90331900acbad105f83a6e82c701c25d7f92836d
kevinsherrell <k.sherrell.1028@gmail.com>
2023-07-06 08:49:13 -0500

    inital spring boot project created

---
+++ mcc_tools/src/main/java/com/dev/mcc_tools/MccToolsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.mcc_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccToolsApplication.class, args);
+	}
+
+}
---
+++ mcc_tools/src/test/java/com/dev/mcc_tools/MccToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.mcc_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
