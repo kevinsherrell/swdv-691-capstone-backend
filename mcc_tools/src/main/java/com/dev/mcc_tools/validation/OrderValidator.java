b7be253dc7cd31a6451bb42af66b3bdb9873a100
kevinsherrell <k.sherrell.1028@gmail.com>
2023-08-15 10:50:40 -0500

    update status route complete

--- mcc_tools/src/main/java/com/dev/mcc_tools/validation/OrderValidator.java
+++ mcc_tools/src/main/java/com/dev/mcc_tools/validation/OrderValidator.java
@@ -9,14 +9,31 @@ public class OrderValidator extends MccValidator {
 
 
     public void checkOrderFormat(Order order) {
-        try {
-            if (!order.getStatus().equals("pending") && !order.getStatus().equals("arrived") && !order.getStatus().equals("delivered")) {
-                setErrors("status", "status must be pending, arrived, or delivered ");
-            }
-            Integer.parseInt(order.getInvoiceNumber());
-        } catch (NumberFormatException e) {
-            setErrors("invoiceNumber", "Invoice number must not contain any letters");
-        }
+//        try {
+//            checkStatus(order.getStatus());
+//            Integer.parseInt(order.getInvoiceNumber());
+//        } catch (NumberFormatException e) {
+//            setErrors("invoiceNumber", "Invoice number must not contain any letters");
+//        }
+        checkStatus(order.getStatus());
+        checkInvoice(order.getInvoiceNumber());
     }
 
+    public Boolean checkStatus(String status) {
+        if (!status.equals("pending") && !status.equals("arrived") && !status.equals("delivered")) {
+            setErrors("status", "status must be pending, arrived, or delivered ");
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean checkInvoice(String invoiceNumber) {
+        try {
+            Integer.parseInt(invoiceNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            setErrors("invoiceNumber", "Invoice number must not contain any letters");
+            return false;
+        }
+    }
 }
