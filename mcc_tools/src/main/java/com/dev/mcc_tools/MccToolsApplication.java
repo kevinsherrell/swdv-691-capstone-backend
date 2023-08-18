b3390fb0f60290e71e9171b05fcf1b40d853fb19
kevinsherrell <k.sherrell.1028@gmail.com>
2023-08-18 18:00:21 -0500

    email working as expected. Beginning to integrate service into routes

--- mcc_tools/src/main/java/com/dev/mcc_tools/MccToolsApplication.java
+++ mcc_tools/src/main/java/com/dev/mcc_tools/MccToolsApplication.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.TimeZone;
 @SpringBootApplication
@@ -18,8 +21,9 @@ public class MccToolsApplication {
 		SpringApplication.run(MccToolsApplication.class, args
 		);
 	}
+
 	@EventListener(ApplicationReadyEvent.class)
 	public void sendMail(){
-		senderService.sendEmail("","test email from application", "this is a test email from capstone project!");
+		senderService.sendEmail(System.getenv("EMAIL_TEST_RECIPIENT"),"test email from application", "this is a test email from capstone project!");
 	}
 }
--- mcc_tools/src/main/java/com/dev/mcc_tools/email/EmailSenderService.java
+++ mcc_tools/src/main/java/com/dev/mcc_tools/email/EmailSenderService.java
@@ -1,24 +1,36 @@
 package com.dev.mcc_tools.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
+
 @Service
-public class EmailSenderService {
-    @Autowired
-    private JavaMailSender mailSender;
+public class EmailSenderService extends JavaMailSenderImpl{
 
     public void sendEmail(String toEmail, String subject, String body) {
+        setHost(System.getenv("EMAIL_HOST"));
+        setPort(Integer.parseInt(System.getenv("EMAIL_PORT")));
+        setUsername(System.getenv("EMAIL_USERNAME"));
+        setPassword(System.getenv("EMAIL_PASSWORD"));
+
+        Properties props = getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("");
+        message.setFrom(System.getenv("EMAIL_TEST_SENDER"));
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
 
-        mailSender.send(message);
-
-
+        this.send(message);
     }
+
 }
