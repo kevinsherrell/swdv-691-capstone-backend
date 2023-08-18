30559dff8204d8bafa55f14f6cdb874388e92f92
kevinsherrell <k.sherrell.1028@gmail.com>
2023-08-18 17:07:52 -0500

    smtp working

--- mcc_tools/src/main/java/com/dev/mcc_tools/MccToolsApplication.java
+++ mcc_tools/src/main/java/com/dev/mcc_tools/MccToolsApplication.java
@@ -1,16 +1,25 @@
 package com.dev.mcc_tools;
 
+import com.dev.mcc_tools.email.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
 import java.util.TimeZone;
 @SpringBootApplication
 public class MccToolsApplication {
 
+	@Autowired
+	private EmailSenderService senderService;
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(MccToolsApplication.class, args
 		);
 	}
-
-
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendEmail("","test email from application", "this is a test email from capstone project!");
+	}
 }
